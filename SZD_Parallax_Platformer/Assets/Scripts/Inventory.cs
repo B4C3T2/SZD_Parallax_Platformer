bbe1bb1908c98f174004ec5e6621430c04b82a1e
WTI59Y <mano201611@gmail.com>
2022-09-30 11:18:58 +0200

    Collectable working in progress

---
+++ SZD_Parallax_Platformer/Assets/Scripts/Inventory.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    /*private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.ComperTag("Collectable"))
+        {
+            Collect(other.GetComponent<CollectManager>());
+        }
+    }
+
+    private void Collect(CollectManager col)
+    {
+        if (col.Collect())
+        {
+            if (col is CollectedKey)
+                Debug.Log("Key collected");
+        }
+    }*/
+}
